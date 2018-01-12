30956571133643e821c8bb25f2834e83aac52b25
nodechessengineserver <nodechessengineserver@gmail.com>
2018-01-12 10:13:29 +0100

    Import from renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+module.exports=exports={}
+
+exports.showInfo=function (){
+	let content=`
+		We are using Node.js ${process.versions.node}
+		Chromium ${process.versions.chrome},
+		and Electron ${process.versions.electron}.
+	`;
+	document.getElementById("sysinfo").innerHTML=content;
+}
